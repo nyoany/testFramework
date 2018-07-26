8ba6fca02c528139055148343960752252a3ca52
Oana Nechiforescu <oana.nechiforescu@gmail.com>
2018-07-26 14:25:38 +0300

    Last changes for reporting.

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,33 +1,18 @@
 import cucumber.api.CucumberOptions;
-import cucumber.api.Scenario;
-import cucumber.api.java.After;
-import cucumber.api.testng.TestNGCucumberRunner;
 import cucumber.api.testng.CucumberFeatureWrapper;
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.testng.ITestResult;
+import cucumber.api.testng.TestNGCucumberRunner;
 import org.testng.annotations.*;
 
-import java.io.File;
-import java.io.IOException;
-
-import static testUtils.WebDriverFactory.getDriver;
 import static testUtils.WebDriverFactory.instantiateDriver;
 import static testUtils.WebDriverFactory.quitDriver;
 
 @CucumberOptions(
             features = "src/test/resources/features",
-            plugin = "json:target/cucumber-report.json",
             glue = {"stepdefs"},
             tags = {"~@Ignore"},
             format = {
                     "pretty",
-                    "html:target/cucumber-reports/cucumber-pretty",
-                    "json:target/cucumber-reports/CucumberTestReport.json",
-                    "json:target/cucumber.json",
-                    "usage:target/cucumber-usage.json",
-                    "rerun:target/cucumber-reports/rerun.txt"
+                    "json:target/cucumber-reports/cucumber.json"
             })
     public class TestRunner {
         private TestNGCucumberRunner testNGCucumberRunner;
